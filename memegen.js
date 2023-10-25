13383bf2a91bd93f307ac18b26a6f1dc28ecec12
jswwWorks <jsww.works@gmail.com>
2023-10-24 21:16:19 -0400

    basic JS code and link JS to HTML file

--- memegen.js
+++ memegen.js
@@ -1,6 +1,19 @@
-// still need to connect js file to html file
+// I'm going to want to add an event to listen for the submitting of that form
+// look up how forms work in js
 
+function makeMeme() {
+  console.log('yay');
+  const topText = document.getElementById('top-text');
+  const picForMeme = document.getElementById('image-url');
+  const bottomText = document.getElementById('bottom-text');
+  alert(topText += bottomText);
+}
+
+
+/*
 const newMeme = document.createElement('img'); // creates the image
 newMeme.setAttribute('src', linkHere); // put link here (you'll have a variable name for this, don't worry)
 
 document.body.append(newMeme); // this puts the image after the body; I might want to have this in a div, though
+
+*/
