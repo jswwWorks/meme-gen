c221af235a9c5e693dae56aa74d42e193c04f344
jswwWorks <jsww.works@gmail.com>
2023-10-24 21:45:22 -0400

    link JS file to HTML and add basic events and a few graphic updates

--- memegen.js
+++ memegen.js
@@ -1,19 +1,34 @@
-// I'm going to want to add an event to listen for the submitting of that form
-// look up how forms work in js
+document.addEventListener("DOMContentLoaded", function() {
+  alert('files are linked'); // Eventually I'll get rid of this!
+})
 
-function makeMeme() {
-  console.log('yay');
-  const topText = document.getElementById('top-text');
-  const picForMeme = document.getElementById('image-url');
-  const bottomText = document.getElementById('bottom-text');
-  alert(topText += bottomText);
-}
+const memeRequested = document.getElementById('submit'); //
+memeRequested.addEventListener("click", function() {
+  alert('you submitted the meme');
+  const topText = document.querySelector('#top-text');
+  const picForMeme = document.querySelector('#image-url');
+  const bottomText = document.querySelector('#bottom-text');
+  alert(`${topText} ${picForMeme} ${bottomText}`); // this is the only line that did not run
+  //[object HTMLInputElement] [object HTMLInputElement] [object HTMLInputElement]
+  // is it normal for it to come out like this?
+})
 
 
 /*
+code that might be useful:
+
 const newMeme = document.createElement('img'); // creates the image
 newMeme.setAttribute('src', linkHere); // put link here (you'll have a variable name for this, don't worry)
 
 document.body.append(newMeme); // this puts the image after the body; I might want to have this in a div, though
 
+*/
+
+
+/*
+Progress!
+
+okay first I made sure the HTML and JS files are properly linked
+
+now I want to work on creating and appending an image based on the url providd
 */
