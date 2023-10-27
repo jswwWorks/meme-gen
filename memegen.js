4e6d92803bfb9fa990d29ef4764d8e8bfaeefa31
jswwWorks <jsww.works@gmail.com>
2023-10-27 13:45:40 -0400

    complete the meme generator hooray!

--- memegen.js
+++ memegen.js
@@ -12,7 +12,7 @@ memeRequestForm.addEventListener("submit", function(e) {
 
   let myUrl = imageInput.value;
   memeContainer.style.backgroundImage = `url(${myUrl})`; // should set background to this url
-  memeContainer.style.backgroundSize = 'contain'; //this didn't work
+  memeContainer.style.backgroundSize = 'contain';
   memeContainer.style.height = '400px';
   memeContainer.style.width = '400px';
   memeContainer.style.backgroundPosition = 'center center';
@@ -32,6 +32,13 @@ memeRequestForm.addEventListener("submit", function(e) {
   newBottomText.innerText = bottomTextInput.value; // puts proper text into element
   memeContainer.append(newBottomText); // appends text to same div as images
 
+  // Delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('del'); // gives it this class
+  deleteButton.innerText = 'Delete this Meme';
+  memeContainer.append(deleteButton);
+  deleteButton.addEventListener('click', e => {removeMe(deleteButton) })
+
   // After storing the values of the meme, reset them for next submission:
   imageInput.value = '';
   topTextInput.value = '';
@@ -39,26 +46,6 @@ memeRequestForm.addEventListener("submit", function(e) {
 
 });
 
-  /*
-
-  search MDN for why things came together with backgroundPosition styling
-
-  // Grabs the image
-  const newMeme = document.createElement('img'); // this makes an empty image element for me to put stuff into
-  newMeme.classList.add('theImage'); // adds class for easier css selection
-  newMeme.setAttribute('src', imageInput.value); // inserts link into element;
-  memeContainer.append(newMeme);
-  */
-
-  /*
-  // creating the image to grab its info
-  let myHeight = newMeme.height; // this was returning 0, maybe because I didn't append it to the page?
-  let myWidth = newMeme.width;
-  console.log(myHeight, myWidth);
-  */
-
-  // the test:
-  /*
-  memeContainer.style.height = `${myHeight}px`;
-  memeContainer.style.width = `${myWidth}px`;
-  */
+function removeMe(button) {
+  button.parentElement.remove();
+}
